05b9b6eaf0527d5da53c00bb5f91fe0cda5ab3e7
Rune Petersen <58623004+runembp@users.noreply.github.com>
2021-08-22 17:31:18 +0000

    1

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    line1 = input("Greetings! How are you doing?")
+    line2 = input("Oh, how interesting. Tell me more!")
+    print("Thanks for sharing!")
+
+  
+
 
 if __name__ == '__main__':
     main()
